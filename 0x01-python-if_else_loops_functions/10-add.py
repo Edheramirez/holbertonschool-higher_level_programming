9969b563e3bfe25cef0b7f7aab668ece2f0e582c
Edheramirez <edher.ramirez@outlook.es>
2021-04-28 00:15:31 -0500

    10-add

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,6 +1,3 @@
-Write a function that adds two integers and returns the result.
-
-Prototype: def add(a, b):
-Returns the value of a + b
-You are not allowed to import any module
-You don’t need to understand __import__
+#!/usr/bin/python3
+def add(a, b):
+    return a+b
