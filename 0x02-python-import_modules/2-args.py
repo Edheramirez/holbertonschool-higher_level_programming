3d96ca3f6f6170bf11aa3fd3a9ac25c8b111450f
Edheramirez <edher.ramirez@outlook.es>
2021-04-28 23:10:45 -0500

    2-args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    argulen = len(sys.argv)
+    if (argulen == 1):
+        print("0 arguments.")
+    elif (argulen == 2):
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(argulen - 1))
+    for argc in range(1, argulen):
+        if (argulen > 1):
+            print("{:d}: {:s}".format(argc, sys.argv[argc]))
