fb72c27af3287909e27a9d27a93c924cb0e27492
Edheramirez <edher.ramirez@outlook.es>
2021-05-19 00:53:27 -0500

    1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines size for 0-square"""
+
+
+class Square:
+    """Define class type Square"""
+
+    def __init__(self, size):
+        """private instance attribute"""
+        self.__size = size
