f3e6163c0a2581a2fe64d208269f0b882f5d1068
Edheramirez <edher.ramirez@outlook.es>
2021-05-20 20:31:21 -0500

    fix order width and height

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize values for, width, height"""
 
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
