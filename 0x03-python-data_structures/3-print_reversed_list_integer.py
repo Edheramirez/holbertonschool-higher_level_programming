da9661b5e9b6a65c880c60ae7ded3b270460f485
Edheramirez <edher.ramirez@outlook.es>
2021-07-01 18:10:49 -0500

    fix evaluate not list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -7,5 +7,8 @@ prints all integers of a list, in reverse order
 def print_reversed_list_integer(my_list=[]):
     """define print revers list"""
 
+    if not my_list:
+        return
+
     for number in reversed(my_list):
         print("{:d}".format(number))
