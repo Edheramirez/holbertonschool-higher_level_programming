c4d9233d866f34717900730d674c71b617e680a3
Edheramirez <edher.ramirez@outlook.es>
2021-07-01 16:46:52 -0500

    fix validate list and pep8

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,8 +5,10 @@
 def print_list_integer(my_list=[]):
     """define list integer"""
 
+    if not my_list:
+        return
     for number in my_list:
-        #recorre lista
+        # recorre lista
         if isinstance(number, int):
-            #builtin isinstance veriica si es una instancia de la clase
+            # builtin isinstance veriica si es una instancia de la clase
             print(number)
