e8800b5d5ae61f4a1f7e50608331bedd3d1251e6
Edheramirez <edher.ramirez@outlook.es>
2021-07-01 16:52:54 -0500

    fix the way have to print and pep8

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,10 +5,8 @@
 def print_list_integer(my_list=[]):
     """define list integer"""
 
-    if not my_list:
-        return
     for number in my_list:
         # recorre lista
-        if isinstance(number, int):
-            # builtin isinstance veriica si es una instancia de la clase
-            print(number)
+        # if isinstance(number, int):
+        # builtin isinstance veriica si es una instancia de la clase
+        print("{:d}".format(number))
