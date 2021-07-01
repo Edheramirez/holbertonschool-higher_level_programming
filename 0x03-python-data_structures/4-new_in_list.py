4c02ba67160154782028fa541b0098483996bcb6
Edheramirez <edher.ramirez@outlook.es>
2021-07-01 17:53:51 -0500

    replace element in a copy

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+replaces an element in a list at a specific position
+without modifying the original list
+"""
+
+
+def new_in_list(my_list, idx, element):
+    """define a new elements in a list"""
+
+    new_list = my_list[:]
+
+    if idx < 0:
+        return new_list
+    if idx >= len(my_list):
+        return new_list
+
+    new_list[idx] = element
+    return new_list
