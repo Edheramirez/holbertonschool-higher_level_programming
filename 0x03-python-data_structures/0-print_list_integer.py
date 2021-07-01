26bc4bb1a884b3f0aadc5b2b8dd83aeb909b3fed
Edheramirez <edher.ramirez@outlook.es>
2021-07-01 16:42:14 -0500

    new proyect

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""print all integers"""
+
+
+def print_list_integer(my_list=[]):
+    """define list integer"""
+
+    for number in my_list:
+        #recorre lista
+        if isinstance(number, int):
+            #builtin isinstance veriica si es una instancia de la clase
+            print(number)
