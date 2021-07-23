0e99e9ec06bf8d4a75b287ba04fef380e2c7824c
Edheramirez <edher.ramirez@outlook.es>
2021-07-23 11:53:33 -0500

    chose character to remove

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+removes all characters c and C from a string
+"""
+
+
+def no_c(my_string):
+    """define no c"""
+    new_str = str()
+    for character in my_string:
+        if not (character == "c" or character == "C"):
+            new_str += character
+    print(new_str)
