08e5c9d20d65beb1a4a9376ba1e6a574994bf162
Edheramirez <edher.ramirez@outlook.es>
2021-07-23 12:23:05 -0500

    fix output

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,5 +4,9 @@ removes all characters c and C from a string
 """
 
 def no_c(my_string):
-    new_string = [i for i in my_string if (i is not "c" and i is not "C")]
-    return "".join(new_string)
+    """define no c"""
+    new_str = str()
+    for character in my_string:
+        if not (character == "c" or character == "C"):
+            new_str += character
+    print(new_str)
