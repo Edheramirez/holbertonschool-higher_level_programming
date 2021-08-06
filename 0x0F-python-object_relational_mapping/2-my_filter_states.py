7cd2f7d1020e94163dca932886af38ccc3257ef7
Edheramirez <edher.ramirez@outlook.es>
2021-08-06 17:48:13 -0500

    name matches the argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Lists all states from hbtn_0e_0_usa"""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+                   ORDER BY states.id ASC".format(sys.argv[4]))
+
+    query_rows = cursor.fetchall()
+
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    db.close()
