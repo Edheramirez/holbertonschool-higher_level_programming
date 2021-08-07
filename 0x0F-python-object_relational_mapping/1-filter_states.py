89c6d116d6ce52ed34984af022a8b090de23f944
Edheramirez <edher.ramirez@outlook.es>
2021-08-07 14:23:53 -0500

    fixing continuation line

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
                          user=argv[1], passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
                    ORDER BY states.id ASC")
 
     query_rows = cursor.fetchall()
