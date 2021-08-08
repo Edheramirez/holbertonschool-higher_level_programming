e9277d36f0860a0ff7cfb8a16f68ec4543c2b885
Edheramirez <edher.ramirez@outlook.es>
2021-08-08 00:08:38 -0500

    fix identation

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
 
     Session = sessionmaker(engine)
     session = Session()
-    states = session.query(State).filter\
-             (State.name == sys.argv[4]).order_by(State.id).all()
+    states = session.query(State).filter(
+        State.name == sys.argv[4]).order_by(State.id).all()
 
     if states:
         print("{}".format(states[0].id))
