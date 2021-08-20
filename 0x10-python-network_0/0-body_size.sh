c909b7de3bea6e9589929a0fa236e2f60f24bedd
Edheramirez <edher.ramirez@outlook.es>
2021-08-19 23:29:07 -0500

    new project

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep Content-Length | cut -d' ' -f2 | tr -d '\r'
+curl -sI $1 content-lenght | wc -c
